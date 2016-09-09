00a2503a36f6aa8ccc3816e83e33110ec0b400b4
Andrey Petrov <peanig@gmail.com>
2016-09-09 10:03:56 +0000

    ip for workers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,8 @@ if [ "$1" = 'tests' ]; then
   $SPARK_HOME/sbin/start-slave.sh localhost:7077
   ./sbt/sbt -DsparkVersion=$SPARK_VERSION -Dconfig.file=configs/tests.conf test
 elif [ "$1" = 'master' ]; then
+  export IP=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
+  echo "$IP	master" >> /etc/hosts
   ./mist.sh master --config configs/docker.conf --jar target/scala-*/mist-assembly-*.jar
 elif [ "$1" = 'worker' ]; then
   ./mist.sh worker --runner local --namespace $2 --config configs/docker.conf --jar target/scala-*/mist-assembly-*.jar
